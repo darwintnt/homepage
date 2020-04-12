1f74a3b44426a4f6fba63d65b42a84f6baac0fa7
Darwin Gomez <darwintnt@hotmail.com>
2020-04-12 17:13:24 -0500

    Updates

---
+++ precache-manifest.53a7ba8d6172d9f4437537b0be14f800.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b139f3dfc102d5866854b58616dc37f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5c5df84719fcb374564e",
+    "url": "/static/css/2.8ee8fe37.chunk.css"
+  },
+  {
+    "revision": "dc1972b026210e5b4624",
+    "url": "/static/css/main.35a4d930.chunk.css"
+  },
+  {
+    "revision": "5c5df84719fcb374564e",
+    "url": "/static/js/2.129e23b2.chunk.js"
+  },
+  {
+    "revision": "49c2158902304cb3cd8dd46f8e93fb12",
+    "url": "/static/js/2.129e23b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc1972b026210e5b4624",
+    "url": "/static/js/main.5620e759.chunk.js"
+  },
+  {
+    "revision": "5eacc7dae01835604a9a",
+    "url": "/static/js/runtime-main.a719a00e.js"
+  }
+]);
